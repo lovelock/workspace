4902f0f0559e30c7de9ff124eb329d4603c07787
lovelock <frostwong@gmail.com>
2014-06-03 15:27:29 +0800

    php error

--- php/404.php
+++ php/404.php
@@ -3,5 +3,39 @@ class errorObject
 {
     private $__error;
 
+    public function __construct($error)
+    {
+        $this->__error = $error;
+    }
+
+    public function getError()
+    {
+        return $this->__error;
+    }
 }
+
+class logToConsole
+{
+    private $__errorObject;
+    public function __construct($errorObject)
+    {
+        $this->__errorObject = $errorObject;
+    }
+
+    public function write()
+    {
+        fwrite(STDERR, $this->__errorObject->getError());
+    }
+}
+
+/*
+ *create the new 404 error object
+ */
+$error = new errorObject("404: Not Found");
+
+/*
+ *write the error to the console
+ */
+$log = new logToConsole($error);
+$log->write();
 ?>
