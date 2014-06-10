9ff756ddb743f8e7a8904c8a99a3844c52fe4baf
lovelock <frostwong@gmail.com>
2014-06-10 20:08:46 +0800

    ruby

--- ruby/first.rb
+++ ruby/first.rb
@@ -1,22 +1,87 @@
-#!/usr/bin/env ruby2
+# #!/usr/bin/env ruby2
 
-say = "I love Ruby"
-puts say
+# say = "I love Ruby"
+# puts say
 
-say['love'] = "*love*"
-puts say.upcase
+# say['love'] = "*love*"
+# puts say.upcase
 
-5.times {
-    puts say
-}
+# 5.times {
+    # puts say
+# }
 
-citis = %w[London
-        Oslo
-        Paris
-        Amsterdam
-        Berlin
-]
+# citis = %w[London
+        # Oslo
+        # Paris
+        # Amsterdam
+        # Berlin
+# ]
 
-visited = %w[Berlin Oslo]
+# visited = %w[Berlin Oslo]
 
-puts citis-visited
+# puts citis-visited
+
+# for i in 1..10
+    # puts i
+    # puts "hello, world"
+# end
+
+# 10.times {
+    # puts "hello, world"
+# }
+#
+# 1.upto(10) {
+    # puts "hello, world"
+# }
+#
+# (1..10).each {
+    # puts "hello, world"
+# }
+#
+#
+# my_status = "happy"
+# puts "hello, world" if my_status == "happy"
+#
+=begin
+This is a comment line it explains
+that the next line of code displays
+a welcome message
+=end
+# say = "I love Ruby"
+# puts say
+#
+# x = 5
+# if x > 5 then
+    # puts "x>5"
+# else
+    # puts "x<=5"
+# end
+
+=begin
+# x = false
+# x = 0
+# x = nil
+x = ""
+if x then
+    puts "a"
+else
+    puts "b"
+end
+false and nil are FALSE
+0 and "", are TRUE
+=end
+
+# score = 70
+# result = case score
+         # when 0..40 then "fail"
+         # when 41..60 then "pass"
+         # when 61..70 then "pass with great"
+# end
+# puts result
+#
+#
+# for i in 1..10
+    # puts "hello, world"
+# end
+
+# for i in 1..10 do puts "hello, world" end
