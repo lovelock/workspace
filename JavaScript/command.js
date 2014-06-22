290d710aeb8c4a9d7494f0d5d5d246ec33e42346
lovelock <frostwong@gmail.com>
2014-06-22 22:38:42 +0800

    command pattern

---
+++ JavaScript/command.js
@@ -0,0 +1,16 @@
+var Command = function(execute, [undo], dest, src){
+    this.execute = execute;
+    this.undo = undo;
+    this.dest = dest;
+    this.src = src;
+}
+
+
+var init = function(name){
+    var initList = {
+        'chat': 'chat-plugin',
+        'reply': 'reply-plugin'
+    };
+
+
+}
