b95fcc681510770f6dedf76fad7280591fa50fa8
lovelock <frostwong@gmail.com>
2014-07-17 11:24:19 +0800

    variable scope

---
+++ JavaScript/return.js
@@ -0,0 +1,13 @@
+(function () {
+    var a = 3;
+    if (a > 0 && a < 5)
+        console.log(a);
+        console.log(a);
+        console.log(a);
+        console.log(a);
+        console.log(a);
+        console.log(a);
+        console.log(a);
+    return null;
+    return null;
+})();
---
+++ JavaScript/testVar.js
@@ -0,0 +1,24 @@
+/*
+ * this method is OK, function is not a variable
+ *test();
+ *
+ *function test() {
+ *    console.log('h');
+ *}
+ */
+
+
+/* this is not OK, because test is a variable, it can't be refered before it is defined.
+ *test();
+ *var test = function(){
+ *    console.log('h');
+ *};
+ */
+
+/*
+ * this is OK, it is refered after it is defined.
+ * var test = function(){
+ * console.log('h');
+ * };
+ * test();
+ */
