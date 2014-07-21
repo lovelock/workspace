ae9f7a8d88d1778828000a098ccd852109b4bfaa
lovelock <frostwong@gmail.com>
2014-07-21 11:56:27 +0800

    add

---
+++ JavaScript/array.js
@@ -0,0 +1,18 @@
+/*
+ *var arr = {
+ *    'arr1': 'test1',
+ *    'arr2': 'test2'
+ *};
+ *
+ *for (var i in arr) {
+ *    console.log(arr[i]);
+ *}
+ *
+ *if ('undefined' === typeof a) {
+ *    return a;
+ *}
+ */
+
+for (var i = 2; i--; i < 0) {
+    console.log(i);
+}
