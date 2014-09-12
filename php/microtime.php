0a435865bfcc92b4b265059d4e35fe876e685971
lovelock <frostwong@gmail.com>
2014-09-12 15:20:17 +0800

    add

---
+++ php/microtime.php
@@ -0,0 +1,3 @@
+<?php
+$a = microtime();
+print_r($a);
--- php/rand.php
+++ php/rand.php
@@ -1,3 +1,22 @@
 <?php
-$a = rand(20, 430);
-print_r($a);
+/*
+ *$a = rand(20, 430);
+ *print_r($a);
+ */
+$a = microtime();
+
+function createRandID($m)
+{
+    $arr = range(1, $m);
+    shuffle($arr);
+
+    for($i = 0; $i<=10;$i++) {
+        $n[] = $arr[$i];
+    }
+
+    return implode($n, ',');
+}
+
+echo createRandID(900000);
+echo chr(10);
+echo microtime() - $a;
