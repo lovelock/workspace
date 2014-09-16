e57be1a31a413913525629cc2bbfb8732630fe07
lovelock <frostwong@gmail.com>
2014-09-16 13:51:20 +0800

    add

---
+++ php/array3.php
@@ -0,0 +1,5 @@
+<?php
+$a = array();
+$b = empty($a);
+// print_r($b);
+var_dump($b);
--- php/date.php
+++ php/date.php
@@ -1,3 +1,8 @@
 <?php
-$curDate = date("Y-m-d");
-print_r($curDate);
+$originTime = '2014-9-1';
+$curTime = time();
+// $curDate = time() / (60 * 60 * 24 * 365);
+// print_r(floor($originTime));
+print_r($originTime);
+echo chr(10);
+print_r(floor($curTime));
