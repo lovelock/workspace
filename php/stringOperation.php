a65b35b7919aca67a7ba3e6a740d08aa62163dda
lovelock <frostwong@gmail.com>
2014-10-11 17:05:32 +0800

    add string operation

---
+++ php/stringOperation.php
@@ -0,0 +1,6 @@
+<?php
+#$originalString = 'DiscreteScoreCalculatorPromotionPrice';
+$originalString = 'CurveScoreCalcualtorPhotos';
+$finalString = substr($originalString, 20) . 'Score';
+echo $finalString;
+echo "\n";
