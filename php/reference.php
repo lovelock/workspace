aa5db56aebaa27672cf75747e669825d717fb8f0
lovelock <frostwong@gmail.com>
2014-10-16 15:57:34 +0800

    add function reference

--- php/reference.php
+++ php/reference.php
@@ -1,4 +1,23 @@
 <?php
-$a = $b = 0;
-$a = 2;
-echo $b;
+/*
+ *$a = $b = 0;
+ *$a = 2;
+ *echo $b;
+ */
+
+class Test
+{
+    public function __construct($entity)
+    {
+
+    }
+
+    public function printEntity($entity)
+    {
+        echo $entity;
+    }
+}
+
+$entity = "a";
+$test = new Test($entity);
+$test->printEntity($entity);
